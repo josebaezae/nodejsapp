4208b603ff036ed4384d5364a5ec1fa7fe45e391
josebaezae <36556587+josebaezae@users.noreply.github.com>
2024-05-24 20:19:37 -0600

    Update nodejsDockerDSL.groovy25-05-2024-rev-3
    Update nodejsDockerDSL.groovy25-05-2024-rev-3

--- DSL/nodejsDockerDSL.groovy
+++ DSL/nodejsDockerDSL.groovy
@@ -16,7 +16,7 @@ job('Aplicacion Node.js Docker DSL') {
         dockerBuildAndPublish {
             repositoryName('josebaezae/nodejsapp')
             tag('${GIT_REVISION,length=7}')
-            registryCredentials('docker-hub')
+            registryCredentials('josebaezae')
             forcePull(false)
             createFingerprints(false)
             skipDecorate()
