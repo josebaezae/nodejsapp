13f470222269da08a807952fca822df59df4a887
josebaezae <36556587+josebaezae@users.noreply.github.com>
2024-06-29 01:28:58 -0600

    Update nodejsDSL.groovy

--- DSL/nodejsDSL.groovy
+++ DSL/nodejsDSL.groovy
@@ -1,4 +1,4 @@
-job('Aplicacion Node.js DSL') {
+job('AplicacionNode.jsDSL') {
     description('Aplicación Node JS DSL para el curso de Jenkins')
     scm {
         git('https://github.com/josebaezae/nodejsapp.git', 'master') { node ->
